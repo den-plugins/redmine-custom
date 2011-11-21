20f95fde629804dcc068bc011eb6824275474c33
john robert arriesgado <exst_jarriesgado@yahoo.com>
2011-11-21 14:55:41 +0800

    added All in holidays

--- app/models/holiday.rb
+++ app/models/holiday.rb
@@ -1,6 +1,6 @@
 class Holiday < ActiveRecord::Base
 
-  LOCATIONS = { 1 => 'Manila', 2 => 'Cebu', 3 => 'Cebu/Manila', 4 => 'Australia', 5 => 'US'}
+  LOCATIONS = { 1 => 'Manila', 2 => 'Cebu', 3 => 'Manila & Cebu', 4 => 'Australia', 5 => 'US', 6 => 'All'}
   
   validates_presence_of :title, :description, :location, :event_date
   
