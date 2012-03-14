87edcfc8807e944a1efbacf12d701c32c2d37e0f
lvillasica <lvillasica@lvillasica.(none)>
2012-03-14 15:18:47 +0800

    Optimize Issue page: removed unnecessary operations on showing more history

--- lib/issues_controller_patch.rb
+++ lib/issues_controller_patch.rb
@@ -19,26 +19,26 @@ module IssuesControllerPatch
       @total_journals = @issue.journals.count
       @journals.each_with_index {|j,i| j.indice = i+1}
       @journals.reverse! if User.current.wants_comments_in_reverse_order?
-      @changesets = @issue.changesets
-      @changesets.reverse! if User.current.wants_comments_in_reverse_order?
-      @allowed_statuses = @issue.new_statuses_allowed_to(User.current)
-      @edit_allowed = User.current.allowed_to?(:edit_issues, @project)
-      @logtime_allowed = TimeEntry.logtime_allowed?(User.current, @project)
-      @priorities = Enumeration.priorities
-      @accounting = Enumeration.accounting_types
-      @default = @issue.accounting.id
-      @time_entry = TimeEntry.new
-      @update_options = {'Internal (DEN only)' => 1, 'Include Mystic' => 2}
-
-		  # for feature #8364
-		  issues = @project.issues.collect {|pi| pi.id}.sort
-		  index = issues.index(@issue.id)
-		  if !index.nil?
-			  prv_issue = (index-1 >= 0) ? issues[index-1] : 0
-			  nxt_issue = (index+1 < issues.size) ? issues[index+1] : 0
-		  end
-		  #
 		  if params[:show_more].blank?
+        @changesets = @issue.changesets
+        @changesets.reverse! if User.current.wants_comments_in_reverse_order?
+        @allowed_statuses = @issue.new_statuses_allowed_to(User.current)
+        @edit_allowed = User.current.allowed_to?(:edit_issues, @project)
+        @logtime_allowed = TimeEntry.logtime_allowed?(User.current, @project)
+        @priorities = Enumeration.priorities
+        @accounting = Enumeration.accounting_types
+        @default = @issue.accounting.id
+        @time_entry = TimeEntry.new
+        @update_options = {'Internal (DEN only)' => 1, 'Include Mystic' => 2}
+
+		    # for feature #8364
+		    issues = @project.issues.collect {|pi| pi.id}.sort
+		    index = issues.index(@issue.id)
+		    if !index.nil?
+			    prv_issue = (index-1 >= 0) ? issues[index-1] : 0
+			    nxt_issue = (index+1 < issues.size) ? issues[index+1] : 0
+		    end
+		    #
         respond_to do |format|
           format.html { render :template => 'issues/show.rhtml', :locals => {:prv => prv_issue, :nxt => nxt_issue} } # for feature #8364 - added locals
           format.atom { render :action => 'changes', :layout => false, :content_type => 'application/atom+xml' }
