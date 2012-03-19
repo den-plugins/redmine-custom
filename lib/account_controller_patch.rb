9d360345b90439790fe46af6e74b6c4c727243b1
Stefany Serino <stefanyserino@gmail.com>
2012-03-19 13:28:25 +0800

    fix #53450: change login  error message for locked/archived users

--- lib/account_controller_patch.rb
+++ lib/account_controller_patch.rb
@@ -5,10 +5,29 @@ module AccountControllerPatch
     base.send(:include, InstanceMethods)
     base.class_eval do
       alias_method_chain :show, :archived
+      alias_method_chain :password_authentication, :archived
     end
   end
 
   module InstanceMethods
+    def password_authentication_with_archived
+      user = User.try_to_login_with_custom_error_message(params[:username], params[:password])
+      if user.nil?
+        # Invalid credentials
+        flash.now[:error] = l(:notice_account_invalid_creditentials)
+      elsif user.locked? or user.archived?
+        flash.now[:error] = l(:notice_account_not_active)
+      elsif user.new_record?
+        # Onthefly creation failed, display the registration form to fill/fix attributes
+        @user = user
+        session[:auth_source_registration] = {:login => user.login, :auth_source_id => user.auth_source_id }
+        render :action => 'register'
+      else
+        # Valid user
+        successful_authentication(user)
+      end
+    end
+    
     def show_with_archived
       @user = User.active_and_archived.find(params[:id])
       @custom_values = @user.custom_values
