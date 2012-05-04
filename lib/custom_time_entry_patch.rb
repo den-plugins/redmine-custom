07450a320dff766c179d4f069508f1d49aedffa8
Stefany Serino <stefanyserino@gmail.com>
2012-05-04 15:32:27 +0800

    fix: timelogs validation budget limit

--- lib/custom_time_entry_patch.rb
+++ lib/custom_time_entry_patch.rb
@@ -50,28 +50,31 @@ module Custom
       end
       
       def budget_computation(project_id)
-        bac_amount = project.project_contracts.all.sum(&:amount)
-        contingency_amount = 0
-        actuals_to_date = 0
-        project_budget = 0
+        if issue.acctg_type == Enumeration.find_by_name('Billable').id
+          bac_amount = project.project_contracts.all.sum(&:amount)
+          contingency_amount = 0
+          actuals_to_date = 0
+          project_budget = 0
 
-        pfrom, afrom, pto, ato = project.planned_start_date, project.actual_start_date, project.planned_end_date, project.actual_end_date
-        to = (ato || pto)
+          pfrom, afrom, pto, ato = project.planned_start_date, project.actual_start_date, project.planned_end_date, project.actual_end_date
+          to = (ato || pto)
 
-        if pfrom && to
-          team = project.members.project_team.all
-          reporting_period = (Date.today)
-          forecast_range = get_weeks_range(pfrom, to)
-          actual_range = get_weeks_range((afrom || pfrom), reporting_period)
-          cost = project.monitored_cost(forecast_range, actual_range, team)
-          actual_list = actual_range.collect {|r| r.first }
-          cost.each do |k, v|
-            actuals_to_date += v[:actual_cost] if actual_list.include?(k.to_date)
+          if pfrom && to
+            team = project.members.project_team.all
+            reporting_period = (Date.today)
+            forecast_range = get_weeks_range(pfrom, to)
+            actual_range = get_weeks_range((afrom || pfrom), reporting_period)
+            cost = project.monitored_cost(forecast_range, actual_range, team)
+            actual_list = actual_range.collect {|r| r.first }
+            cost.each do |k, v|
+              actuals_to_date += v[:actual_cost] if actual_list.include?(k.to_date)
+            end
+            project_budget = bac_amount + contingency_amount
           end
-          project_budget = bac_amount + contingency_amount
-        end
-        if (project_budget - actuals_to_date) < 0 && issue.acctg_type == Enumeration.find_by_name('Billable').id
-          errors.add_to_base l(:error_timelog_budget_consumed)
+          #include current time entry's amount
+          member = team.detect{|m| m.user_id == user_id}
+          actuals_to_date += hours * member.internal_rate
+          errors.add_to_base l(:error_timelog_budget_consumed) if (project_budget - actuals_to_date) < 0
         end
       end
     end
