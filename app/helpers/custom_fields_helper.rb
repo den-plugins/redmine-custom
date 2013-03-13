0e68c1d481db1ffc8e08851294068e41d75352f6
Jayson Larin <jaysonlarin@yahoo.com>
2013-03-13 14:43:56 +0800

    [#69651] include admin calendar for time log admin

--- app/helpers/custom_fields_helper.rb
+++ app/helpers/custom_fields_helper.rb
@@ -51,11 +51,13 @@ module CustomFieldsHelper
   end
 
   def admin_lock_time_logging(cv)
+    calendar = User.current.time_logs_admin? ? calendar_for("project_lock_time_logging") : date_calendar_for("project_lock_time_logging",cv.customized)
     default_date = cv.customized && cv.customized.lock_time_logging ? cv.customized.lock_time_logging  : Date.today.end_of_year.last_year
     html = ''
     html << "<span id='lock_time_logging' style='padding-left:30px'><b>Time logging locked until</b>"
     html << text_field_tag("project[lock_time_logging]", default_date, :readonly => true, :size => 10)
-    html << date_calendar_for("project_lock_time_logging",cv) + "</span>"
+    html << calendar
+    html << "</span>"
   end
 
   # Return custom field label tag
