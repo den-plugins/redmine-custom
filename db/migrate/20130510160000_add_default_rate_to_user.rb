b0f5abe119a2574211fe8eb893a7a19f60864455
Geraldine Milan <blackgemph@yahoo.com>
2013-05-10 16:11:09 +0800

    add default rate to schema

---
+++ db/migrate/20130510160000_add_default_rate_to_user.rb
@@ -0,0 +1,11 @@
+class AddDefaultRateToUser < ActiveRecord::Migration
+  def self.up
+    add_column :users, :deafult_rate, :float
+    add_column :users, :effective_date, :date
+  end
+
+  def self.down
+    remove_column :users, :default_rate
+    remove_column :users, :effective_date
+  end
+end
