3aaf175b0ae31861b199e2f5fec947f568360ec2
Geraldine Milan <blackgemph@yahoo.com>
2013-05-10 16:27:25 +0800

    Fix typo error

--- db/migrate/20130510160000_add_default_rate_to_user.rb
+++ db/migrate/20130510160000_add_default_rate_to_user.rb
@@ -1,6 +1,6 @@
 class AddDefaultRateToUser < ActiveRecord::Migration
   def self.up
-    add_column :users, :deafult_rate, :float
+    add_column :users, :default_rate, :float
     add_column :users, :effective_date, :date
   end
 
